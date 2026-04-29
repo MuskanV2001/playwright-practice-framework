729c2ddea44bc40024a96ffb0c0d0cd07d61f6c8
Muskan Vishwakarma <muskanv01lko@gmail.com>
2026-04-29 17:13:10 +0530

    Added Allure reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,23 +11,26 @@ const testDir = defineBddConfig({
 
 export default defineConfig({
   testDir,
-  /* Run tests in files in parallel */
+
   fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  reporter: [
+    ['dot'],
+    ['html'],
+    ['json' , { outputFile: 'test-results/cucumber-report.json'}],
+    ['allure-playwright']
+  ],
+
   use: {
     headless: false,
     trace: 'retain-on-failure',
+    screenshot: 'only-on-failure'
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
